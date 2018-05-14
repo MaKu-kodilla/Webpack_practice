c82fd66b82f8ff814d18ccb3554020fdcc0dda37
Maciej Kubas <maciekkubas@gmail.com>
2018-05-14 21:57:30 +0200

    corrected

--- Zadanie 16.1 Pierwsza konfiguracja Webpacka/webpack.config.js
+++ Zadanie 16.1 Pierwsza konfiguracja Webpacka/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, './build'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     }
 };
